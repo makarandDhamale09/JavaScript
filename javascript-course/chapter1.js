7ebfc08c385ab4a65fd9100a65848e330b412170
makaranddhamale <makarand.dhamale09@gmail.com>
2024-04-29 22:48:29 +0530

    Added some method types and a puzzle

--- javascript-course/chapter1.js
+++ javascript-course/chapter1.js
@@ -48,3 +48,69 @@ var landscape = function () {
 };
 
 console.log(landscape());
+console.log("----------");
+
+// Closure
+function wrapValues(n) {
+  var localVariable = n;
+  return function () {
+    return localVariable;
+  };
+}
+
+var wrap1 = wrapValues(1); // here wrap1 is the inner function which returns the localvariable
+var wrap2 = wrapValues(2);
+
+console.log(wrap1());
+console.log(wrap2());
+
+/**
+ * This method returns a method which can me multiplied with the factor with which it was called
+ * @param {factor} factor
+ * @returns fuction which multiplies the factor with the passed number
+ */
+function multiplier(factor) {
+  return function (number) {
+    return number * factor;
+  };
+}
+
+var twice = multiplier(2); // here twice is a function that is returned which is inner function of multiplier
+console.log(twice); // this will print the method as anonymous as it is the inner function
+console.log(twice(5));
+
+// Recursion
+function power(base, exponent) {
+  if (exponent == 0) return 1;
+  else return base * power(base, exponent - 1);
+}
+
+console.log(`The value of 2 to the power 4 is ${power(2, 4)}`);
+
+/**
+ * Consider this puzzle: by starting from the number 1 and
+ * repeatedly either adding 5 or multiplying by 3, an
+ * infinite amount of new numbers can be produced.
+ * How would you write a function that, given a number,
+ * tries to find a sequence of such additions and
+ * multiplications that produce that number? For example,
+ * the number 13 could be reached by first multiplying
+ * by 3 and then adding 5 twice, whereas the number 15
+ * cannot be reached at all.
+ *
+ * Below is recursive solution
+ */
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) return history;
+    else if (current > target) return null;
+    else
+      return (
+        find(current + 5, "(" + history + " + 5)") ||
+        find(current * 3, "(" + history + " * 3)")
+      );
+  }
+  return find(1, "1");
+}
+
+console.log(`History of solution is ${findSolution(24)}`);
